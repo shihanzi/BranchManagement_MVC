3e8f0d3176edaa42ab06f759561b47f09ff9f114
shihanze <shihanmails@gmail.com>
2025-10-27 19:01:15 +0530

    user add

---
+++ Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using AhadiyyaMVC.DataAccess;
+using AhadiyyaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AhadiyyaMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserRepository _userRepo;
+        public UserController(UserRepository user)
+        {
+            _userRepo = user;
+        }
+        public IActionResult Index()
+        {
+            //int? districtId = HttpContext.Session.GetInt32("DistrictId");
+            //int? branchId = HttpContext.Session.GetInt32("BranchId");
+
+            var list = _userRepo.GetUsers();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Roles = _userRepo.GetRoles();               // returns Id + Name
+            ViewBag.Districts = _userRepo.GetDistricts();   // returns Id + Name
+            ViewBag.Branches = _userRepo.GetBranches();       // returns Id + Name
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User model)
+        {
+            int? roleId = HttpContext.Session.GetInt32("RoleId");
+            int? sessionDistrict = HttpContext.Session.GetInt32("DistrictId");
+
+            // Branch Admins not allowed
+            if (roleId == 3)
+                return Forbid();
+
+            // Enforce DistrictAdmin restriction: they can only create branches in their own district
+            if (roleId == 2 && sessionDistrict.HasValue && model.DistrictId != sessionDistrict.Value)
+            {
+                ModelState.AddModelError("", "District Admin can only create branches in their own district.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                //ViewBag.Districts = (roleId == 2 && sessionDistrict.HasValue)
+                //    ? _userRepo.GetDistricts().Where(d => d.Id == sessionDistrict.Value).ToList()
+                //    : _userRepo.GetDistricts();
+
+                return View(model);
+            }
+
+            _userRepo.AddBranch(model); // implement in repo
+            return RedirectToAction("Index");
+        }
+    }
+}
--- DataAccess/UserRepository.cs
+++ DataAccess/UserRepository.cs
@@ -6,7 +6,15 @@ namespace AhadiyyaMVC.DataAccess
     public class UserRepository
     {
         private readonly DbConnectionHelper _db;
-        public UserRepository(DbConnectionHelper db) => _db = db;
+        private readonly BranchRepository _branch;
+        private readonly DistrictRepository _district;
+
+        public UserRepository(DbConnectionHelper db, DistrictRepository district, BranchRepository branch)
+        {
+            _db = db;
+            _branch = branch;
+            _district = district;
+        }
 
         public User? GetUser(string username, string password)
         {
@@ -34,6 +42,131 @@ namespace AhadiyyaMVC.DataAccess
                 DistrictId = reader.IsDBNull(5) ? null : reader.GetInt32(5),
                 BranchId = reader.IsDBNull(6) ? null : reader.GetInt32(6)
             };
+        }
+
+        public List<User> GetUsers()
+        {
+            var users = new List<User>();
+
+            using (var conn = _db.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT Id,Username FROM Users";
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            users.Add(new User
+                            {
+                                Id = reader.GetInt32(0),
+                                Username = reader.GetString(1),
+                            });
+                        }
+                    }
+                }
+            }
+            return users;
+        }
+
+        public List<District> GetDistricts()
+        {
+            var districts = new List<District>();
+
+            using (var conn = _db.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT Id,Name FROM Districts";
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            districts.Add(new District
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                            });
+                        }
+                    }
+                }
+            }
+            return districts;
+        }
+
+        public List<Role> GetRoles()
+        {
+            var roles = new List<Role>();
+
+            using (var conn = _db.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT id,name FROM Roles";
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            roles.Add(new Role
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                            });
+                        }
+                    }
+                }
+            }
+            return roles;
+        }
+
+        public List<Branch> GetBranches()
+        {
+            var branches = new List<Branch>();
+
+            using (var conn = _db.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT Id,Name FROM branches";
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            branches.Add(new Branch
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                            });
+                        }
+                    }
+                }
+            }
+            return branches;
+        }
+
+        public void AddBranch(User model)
+        {
+            using (var conn = _db.GetConnection())
+            {
+                conn.Open();
+                string sql = "INSERT INTO users (Username,PasswordHash,RoleId,DistrictId,BranchId,CreatedDate,IsActive) VALUES (@Username,@PasswordHash,@RoleId,@DistrictId,@BranchId,@CreatedDate,@IsActive)";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.PasswordHash);
+                    cmd.Parameters.AddWithValue("@Username", model.Username);
+                    cmd.Parameters.AddWithValue("@PasswordHash", hashedPassword);
+                    cmd.Parameters.AddWithValue("@RoleId", model.RoleId);
+                    cmd.Parameters.AddWithValue("@DistrictId", model.DistrictId);
+                    cmd.Parameters.AddWithValue("@BranchId", model.BranchId);
+                    cmd.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+                    cmd.Parameters.AddWithValue("@IsActive", 1);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
